6eccc08c6b18aeac674faf3dc836ff08d7957531
Andrew043y <andrew043y@yahoo.com>
2024-10-26 15:36:14 -0500

    Update MainActivity.java

--- app/src/main/java/edu/utsa/predictionapp/Controllers/MainActivity.java
+++ app/src/main/java/edu/utsa/predictionapp/Controllers/MainActivity.java
@@ -1,1 +1,33 @@
+package edu.utsa.predictionapp;
 
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> predictPhrases; 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void addPrediction(String p){
+        predictPhrases.add(p);
+    }
+    
+    public getQuestion(int i){
+        return predictPhrases.get(i);
+    }
+}
