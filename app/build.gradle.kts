ed5841ef4f0799093b4251257805c14b64061c8c
Perry <78666324+MasterTrainerPK@users.noreply.github.com>
2024-10-26 15:38:43 -0500

    Add MongoDB
    it's in the build.graldle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.mongodb.driver.sync)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
