f0f51a5c8c52507919507574eabb8e8a8c44e9cb
Perry <78666324+MasterTrainerPK@users.noreply.github.com>
2024-10-26 19:58:32 -0500

    MainActivity setcontent

--- app/src/main/java/edu/utsa/predictionapp/Controllers/MainActivity.java
+++ app/src/main/java/edu/utsa/predictionapp/Controllers/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.predictButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
