5de68d8ec6b59db12b6081a64574f4bae34cc49a
Perry <78666324+MasterTrainerPK@users.noreply.github.com>
2024-10-26 19:16:46 -0500

    further messing with layouts
    the main activity is partially complete
    (sry andrew)

--- app/src/main/java/edu/utsa/predictionapp/Controllers/MainActivity.java
+++ app/src/main/java/edu/utsa/predictionapp/Controllers/MainActivity.java
@@ -3,34 +3,34 @@ package edu.utsa.predictionapp.Controllers;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
+
 import java.util.ArrayList;
 
 import edu.utsa.predictionapp.R;
 
-public class MainActivity extends AppCompatActivity { 
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-        final Button button = findViewById(R.id.predictButton);
+        Button button = findViewById(R.id.predictButton);
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
+            public void onClick(View v) {
+                TextInputEditText inputText = findViewById(R.id.userInput);
+                inputText.getText();
                 //insert code for when button is pressed
             }
         });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
